db59b4fd7ebea5c37ac9a38040591bb55aad80b5
limengxu <1354786446@qq.com>
2023-03-04 17:07:45 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const name = 'lmx'
+window.console.log(name)
+</script>
+
+<template>
+  <div class="app">
+    <h2>App</h2>
+  </div>
+</template>
+
+<style scoped>
+</style>
