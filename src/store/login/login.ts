33b0870c428462a7253a76a266b20edf53f8d640
limengxu <1354786446@qq.com>
2023-03-05 22:17:07 +0800

    本地缓存和缓存工具的封装

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -1,21 +1,26 @@
 import { defineStore } from 'pinia'
 import { accountLoginRequest } from '@/service/login/login'
 import type { IAccount } from '@/types'
+import { localCache } from '@/utils/cache'
+import { LOGIN_TOKEN } from '@/global/constants'
 
 const useLoginStore = defineStore('login', {
   state: () => {
     return {
       id: '',
       name: '',
-      token: ''
+      token: localCache.getCache(LOGIN_TOKEN) ?? ''
     }
   },
   actions: {
     async loginAccountAction(account: IAccount) {
+      // 1账号登录，获取token等信息
       const loginRes = await accountLoginRequest(account)
       this.id = loginRes.data.id
       this.name = loginRes.data.name
       this.token = loginRes.data.token
+      // 2进行本地缓存
+      localCache.setCache(LOGIN_TOKEN, this.token)
     }
   }
 })
