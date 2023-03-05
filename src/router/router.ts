5ecca565fc5b376973eadfdefb108c6af4baf882
limengxu <1354786446@qq.com>
2023-03-05 22:47:20 +0800

    完成登录成功后的路由导航守卫

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import { LOGIN_TOKEN } from '@/global/constants'
+import { localCache } from '@/utils/cache'
 
 export const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
       path: '/',
-      redirect: '/login'
+      redirect: '/main'
     },
     {
       path: '/login',
@@ -23,3 +25,9 @@ export const router = createRouter({
 })
 
 // 导航守卫
+router.beforeEach((to) => {
+  const token = localCache.getCache(LOGIN_TOKEN)
+  if (to.path === '/main' && !token) {
+    return '/login'
+  }
+})
