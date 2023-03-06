82395bf02cfba8c1715cef4a02c3dd80a433ad4d
limengxu <1354786446@qq.com>
2023-03-06 22:45:23 +0800

    动态加载所有的路由对象文件

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia'
+import type { RouteRecordRaw } from 'vue-router'
 import { accountLoginRequest, getUserInfoById, getUserMenusByRoleId } from '@/service/login/login'
 import type { IAccount } from '@/types'
 import { localCache } from '@/utils/cache'
@@ -39,6 +40,23 @@ const useLoginStore = defineStore('login', {
       // 4进行本地缓存
       localCache.setCache('userInfo', userInfo)
       localCache.setCache('userMenus', userMenus)
+
+      // 重要:动态的添加路由
+      // 1.动态获取所有的路由对象，放到数组中
+      // * 路由对象都在独立的文件中
+      // * 从文件中将所有路由对象先读取数组中
+      const localRoutes: RouteRecordRaw[] = []
+      // 1.1 读取router/main所有的ts文件
+      const files: Record<string, any> = import.meta.glob('../../router/main/**/*.ts', {
+        eager: true
+      })
+      for (const key in files) {
+        const module = files[key]
+        localRoutes.push(module.default)
+      }
+
+      // 2.根据菜单去匹配正确的路由
+      // * router.addRoute( 'main ' , xxX)
 
       // 5页面跳转（main页面）
       router.push('/main')
