15ba94b9a22be6980d4e4911afe299e707e8d5a6
limengxu <1354786446@qq.com>
2023-03-07 17:38:26 +0800

    user页面-search重置和查询的点击处理

--- src/views/main/system/user/c-cpns/user-search.vue
+++ src/views/main/system/user/c-cpns/user-search.vue
@@ -1,35 +1,36 @@
 <template>
   <div class="user-search">
     <!-- 输入搜索关键字的表单 -->
-    <el-form label-width="80px" size="large">
+    <el-form ref="formRef" :model="serachForm" label-width="80px" size="large">
       <el-row :gutter="20">
         <el-col :span="8">
-          <el-form-item label="用户名">
-            <el-input placeholder="请输入查询的用户名" />
+          <el-form-item label="用户名" prop="name">
+            <el-input v-model="serachForm.name" placeholder="请输入查询的用户名" />
           </el-form-item>
         </el-col>
         <el-col :span="8">
-          <el-form-item label="真实姓名">
-            <el-input placeholder="请输入查询的真实姓名" />
+          <el-form-item label="真实姓名" prop="realname">
+            <el-input v-model="serachForm.realname" placeholder="请输入查询的真实姓名" />
           </el-form-item>
         </el-col>
         <el-col :span="8">
-          <el-form-item label="电话号码">
-            <el-input placeholder="请输入查询的电话号码" />
+          <el-form-item label="电话号码" prop="cellphone">
+            <el-input v-model="serachForm.cellphone" placeholder="请输入查询的电话号码" />
           </el-form-item>
         </el-col>
 
         <el-col :span="8">
-          <el-form-item label="状态">
-            <el-select placeholder="请选择查询的状态" style="width: 100%">
-              <el-option label="启用" value="1" />
-              <el-option label="禁用" value="0" />
+          <el-form-item label="状态" prop="enable">
+            <el-select v-model="serachForm.enable" placeholder="请选择查询的状态" style="width: 100%">
+              <el-option label="启用" :value="1" />
+              <el-option label="禁用" :value="0" />
             </el-select>
           </el-form-item>
         </el-col>
         <el-col :span="8">
-          <el-form-item label="创建时间">
+          <el-form-item label="创建时间" prop="createAt">
             <el-date-picker
+              v-model="serachForm.createAt"
               type="daterange"
               range-separator="-"
               start-placeholder="开始时间"
@@ -42,13 +43,27 @@
 
     <!-- 重置和查询按钮 -->
     <div class="btns">
-      <el-button icon="Refresh">重置</el-button>
+      <el-button icon="Refresh" @click="handleResetClick">重置</el-button>
       <el-button icon="Search" type="primary">查询</el-button>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { FormInstance } from 'element-plus'
+import { reactive, ref } from 'vue'
+
+const serachForm = reactive({
+  name: '',
+  realname: '',
+  cellphone: '',
+  enable: 1,
+  createAt: ''
+})
+const formRef = ref <FormInstance>()
+function handleResetClick() {
+  formRef.value?.resetFields()
+}
 </script>
 
 <style lang="less" scoped>
