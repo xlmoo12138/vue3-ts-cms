06b31153f24cff935cbe92c9fd0d2b32a9fcd86c
limengxu <1354786446@qq.com>
2023-03-07 13:48:13 +0800

    user页面-整体布局和search组件抽取

---
+++ src/views/main/system/user/c-cpns/user-search.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="user-search">
+    <h2>user-search</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="less" scoped>
+.user-search {
+  color: red;
+  background-color: #fff;
+  padding: 20px;
+}
+</style>
