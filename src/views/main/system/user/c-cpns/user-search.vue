affecd66f3b3903ae7f7406ec279d006eaa8d357
limengxu <1354786446@qq.com>
2023-03-07 16:45:05 +0800

    user页面-search的表单搜索框界面搭建

--- src/views/main/system/user/c-cpns/user-search.vue
+++ src/views/main/system/user/c-cpns/user-search.vue
@@ -1,6 +1,44 @@
 <template>
   <div class="user-search">
-    <h2>user-search</h2>
+    <el-form label-width="80px" size="large">
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-form-item label="用户名">
+            <el-input placeholder="请输入查询的用户名" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="真实姓名">
+            <el-input placeholder="请输入查询的真实姓名" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="电话号码">
+            <el-input placeholder="请输入查询的电话号码" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-form-item label="状态">
+            <el-select placeholder="请选择状态" style="width: 100%">
+              <el-option label="启用" value="1" />
+              <el-option label="禁用" value="0" />
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="创建时间">
+            <el-date-picker
+              type="daterange"
+              range-separator="-"
+              start-placeholder="开始时间"
+              end-placeholder="结束时间"
+            />
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
   </div>
 </template>
 
@@ -12,5 +50,10 @@
   color: red;
   background-color: #fff;
   padding: 20px;
+
+  .el-form-item {
+    padding: 20px 30px;
+    margin-bottom: 0;
+  }
 }
 </style>
