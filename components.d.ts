bcb17701470613334e223e45eaf18910dd23eda3
limengxu <1354786446@qq.com>
2023-03-07 21:27:01 +0800

    user页面-content中的table数据

--- components.d.ts
+++ components.d.ts
@@ -38,6 +38,8 @@ declare module '@vue/runtime-core' {
     ElSelect: typeof import('element-plus/es')['ElSelect']
     ElSubMen: typeof import('element-plus/es')['ElSubMen']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
+    ElTable: typeof import('element-plus/es')['ElTable']
+    ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
     ElTabPane: typeof import('element-plus/es')['ElTabPane']
     ElTabs: typeof import('element-plus/es')['ElTabs']
     HeaderCrumb: typeof import('./src/components/main-header/c-cpns/header-crumb.vue')['default']
--- src/views/main/system/user/c-cpns/user-content.vue
+++ src/views/main/system/user/c-cpns/user-content.vue
@@ -5,9 +5,20 @@
       <el-button type="primary">新建用户</el-button>
     </div>
     <div class="table">
-      <ul>
-        <li v-for="item in usersList" :key="item.id">{{ item.name }}</li>
-      </ul>
+      <el-table :data="usersList" border style="width: 100%">
+        <el-table-column align="center" type="selection" label="选择" />
+        <el-table-column align="center" type="index" label="序号" width="80px" />
+        <el-table-column align="center" prop="name" label="用户名" width="150px" />
+        <el-table-column align="center" prop="realname" label="真实姓名" width="150px" />
+        <el-table-column align="center" prop="cellphone" label="手机号码" width="150px" />
+        <el-table-column align="center" prop="enable" label="状态" width="100px" />
+        <el-table-column align="center" prop="createAt" label="创建时间" />
+        <el-table-column align="center" prop="createAt" label="更新时间" />
+        <el-table-column align="center" label="操作" width="150px">
+          <el-button size="small" icon="Edit" type="primary" text>编辑</el-button>
+          <el-button size="small" icon="delete" type="danger" text>删除</el-button>
+        </el-table-column>
+      </el-table>
     </div>
     <div class="pagination">分页</div>
   </div>
@@ -38,6 +49,17 @@ const { usersList } = storeToRefs(systemStore)
 
     .title {
       font-size: 22px;
+    }
+  }
+
+  .table {
+    :deep(.el-table__cell) {
+      padding: 14px 0;
+    }
+
+    .el-button {
+      margin-left: 0;
+      padding: 5px 8px;
     }
   }
 }
