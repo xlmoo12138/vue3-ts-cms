f0bbe9e5b7badf2358f93b9ee4921e073c2b393b
limengxu <1354786446@qq.com>
2023-03-09 16:01:35 +0800

    角色页面-高阶组件快速搭建部门页面

--- src/views/main/system/role/role.vue
+++ src/views/main/system/role/role.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="role">
-    <page-search :search-config="searchConfig" />
+    <page-search :search-config="searchConfig" @query-click="handleQueryClick" @reset-click="handleResetClick" />
+    <page-content ref="contentRef" :content-config="contentConfig" @new-click="handleNewClick" @edit-click="handleEditClick" />
+    <page-modal ref="modalRef" :modal-config="modalConfig" />
   </div>
 </template>
 
 <script setup lang="ts">
 import searchConfig from './config/search.config'
+import contentConfig from './config/content.config'
+import modalConfig from './config/modal.config'
 import PageSearch from '@/components/page-search/page-search.vue'
+import PageContent from '@/components/page-content/page-content.vue'
+import PageModal from '@/components/page-modal/page-modal.vue'
+import usePageContent from '@/hooks/usePageCotent'
+import usePageModal from '@/hooks/usePageModal'
+
+// 逻辑关系
+const { contentRef, handleQueryClick, handleResetClick } = usePageContent()
+
+const { modalRef, handleNewClick, handleEditClick } = usePageModal()
 </script>
 
 <style lang="less" scoped>
-.role {
-}
 </style>
