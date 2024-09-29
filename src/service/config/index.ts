4f4b812fc992e6f1172038db29b4997c0be7521e
moe <kirdoyz@gmail.com>
2024-09-30 00:28:04 +0800

    update develop BASE_URL

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -5,7 +5,8 @@ if (import.meta.env.PROD) {
   BASE_URL = 'http://152.136.185.210:4000'
 } else {
   // 开发环境
-  BASE_URL = 'http://152.136.185.210:5000'
+  // BASE_URL = 'http://152.136.185.210:5000'
+  BASE_URL = 'http://123.207.32.32:5000'
 }
 
 export const TIME_OUT = 10000
